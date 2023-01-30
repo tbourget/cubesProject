dc5660900f24e5b20909cf28f24185215ca5d65c
tbourget <61435284+tbourget@users.noreply.github.com>
2023-01-30 13:34:47 -0500

    Attempt at creating a json file out of the requests response

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from requests.auth import HTTPBasicAuth
 
 
 def main():
-    entries = get_entries_as_JSON()
-    print(json.dumps(entries.json(), indent=2))
+    entries = get_entries_as_json()
+    print(json.dumps(entries, indent=2))
+    write_json_to_file(entries, 'entries')
 
 
 def retrieve_api_key_from_secrets(api_name: str):
@@ -27,7 +28,7 @@ def retrieve_api_key_from_secrets(api_name: str):
         return data[api_name]
 
 
-def get_entries_as_JSON():
+def get_entries_as_json():
     """
     Retrieves entries from the CUBES project proposal form as a JSON file
 
@@ -36,13 +37,27 @@ def get_entries_as_JSON():
     json
         The entries from the CUBES form as a JSON
     """
-    # Use a breakpoint in the code line below to debug your script.
     form_url = 'https://tbourget.wufoo.com/api/v3/forms/z1x6vy9k0dlvbjl/entries.json?sort=EntryId&sortDirection=DESC'
     username = retrieve_api_key_from_secrets('tbourget_CUBES')
     password = 'tbourget_CUBES'
 
     response = requests.get(form_url, auth=HTTPBasicAuth(username, password))
     return response.json()
+
+
+def write_json_to_file(json: str, file_name: str):
+    """
+    Writes a JSON object 'json' to a file named 'file_name'
+
+    Parameters
+    ----------
+    json : str
+        JSON object to write to a file
+    file_name
+        Desired name for JSON file
+    """
+    with open(file_name + '.json', 'w') as f:
+        json.dump(json, f)
 
 
 if __name__ == '__main__':
