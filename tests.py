02ec3e2a6a96b62edab246cc68babc67d8df0258
tbourget <61435284+tbourget@users.noreply.github.com>
2023-01-30 18:17:41 -0500

    Saves entries to JSON file with an indented format, added 1 pytest

--- tests.py
+++ tests.py
@@ -2,6 +2,7 @@ import pytest
 from testfixtures import TempDirectory
 from main import *
 
+
 def test_retrieve_api_key_from_secrets(capfd):
     """
     Test function for retrieve_api_key_from_secrets()
