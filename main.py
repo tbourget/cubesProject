6b9462d082d916666335aed8d629bcb28271a63d
tbourget <61435284+tbourget@users.noreply.github.com>
2023-01-30 12:53:04 -0500

    Put API key in .secrets folder, retrieves entries as a JSON and prints them

--- main.py
+++ main.py
@@ -2,15 +2,34 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import json
+import requests
+from requests.auth import HTTPBasicAuth
 
 
-def print_hi(name):
+def main():
+
+    form_url = 'https://tbourget.wufoo.com/api/v3/forms/z1x6vy9k0dlvbjl/entries.json?sort=EntryId&sortDirection=DESC'
+    username = retrieve_api_key('tbourget_CUBES')
+    password = 'tjbourget'
+
+    response = requests.get(form_url, auth=HTTPBasicAuth(username, password))
+
+    print(json.dumps(response.json(), indent=2))
+
+def retrieve_api_key(username):
+    with open(".secrets.json") as f:
+
+        data = json.load(f)
+        return data[username]
+
+#def get_entries_as_JSON():
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    # GET http://{subdomain}.wufoo.com/api/v3/forms/{identifier}/entries.{format}
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
