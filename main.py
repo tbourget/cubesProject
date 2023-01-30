7f8c880610ee6f7eb25755a9ff55db7c85056f74
tbourget <61435284+tbourget@users.noreply.github.com>
2023-01-30 13:17:09 -0500

    Added function docstrings

--- main.py
+++ main.py
@@ -1,35 +1,49 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import json
 import requests
 from requests.auth import HTTPBasicAuth
 
 
 def main():
+    entries = get_entries_as_JSON()
+    print(json.dumps(entries.json(), indent=2))
 
+
+def retrieve_api_key(api_name: str):
+    """
+    Retrieves an API key from a .secrets JSON dict using an API name
+
+    Parameters
+    ----------
+    api_name : str
+        Name of the API to retrieve the key for
+
+    Returns
+    -------
+    str
+        The API key of the corresponding api_name
+    """
+    with open(".secrets.json") as f:
+        data = json.load(f)
+        return data[api_name]
+
+
+def get_entries_as_JSON():
+    """
+    Retrieves entries from the CUBES project proposal form as a JSON file
+
+    Returns
+    -------
+    json
+        The entries from the CUBES form as a JSON
+    """
+    # Use a breakpoint in the code line below to debug your script.
     form_url = 'https://tbourget.wufoo.com/api/v3/forms/z1x6vy9k0dlvbjl/entries.json?sort=EntryId&sortDirection=DESC'
     username = retrieve_api_key('tbourget_CUBES')
-    password = 'tjbourget'
+    password = 'tbourget_CUBES'
 
     response = requests.get(form_url, auth=HTTPBasicAuth(username, password))
-
-    print(json.dumps(response.json(), indent=2))
-
-def retrieve_api_key(username):
-    with open(".secrets.json") as f:
-
-        data = json.load(f)
-        return data[username]
-
-#def get_entries_as_JSON():
-    # Use a breakpoint in the code line below to debug your script.
-    # GET http://{subdomain}.wufoo.com/api/v3/forms/{identifier}/entries.{format}
+    return response.json()
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
